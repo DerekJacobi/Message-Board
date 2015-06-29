61b2d7f9f1ea11a605d664505a673afdc7b4c010
Derek Jacobi <derekjacobi@gmail.com>
2015-06-29 10:22:14 -0400

    sessions initial commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express     = require('express'),
     ejs         = require('ejs'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
+    methodOverride = require('method-override')
     session     = require('express-session');
 
 server.set('views', './views');
@@ -12,8 +13,8 @@ server.set('view engine', 'ejs');
 
 server.use(session({
   secret: 'wdiarcher',
-  resave: false,
-  saveUninitialized: false
+  resave: true,
+  saveUninitialized: true
 }));
 
 //form submission
@@ -26,11 +27,19 @@ server.use(function(req, res, next) {
   next();
 });
 
-server.use(function(req, res, next) {
-  req.session.viewCount = req.session.viewCount || 0;
-  req.session.viewCount++;
-  console.log("Views", req.session.viewCount);
-  res.end();
+server.get('/', function(req, res, next){
+  res.render('index', {
+    username: req.session.username,
+    password: req.session.password,
+    email: req.session.email
+  });
+});
+
+server.post('/', function(req, res, next) {
+  req.session.username = req.body.username;
+  req.session.password = req.body.password;
+  req.session.email = req.body.email;
+  res.redirect(301, '/');
 });
 
 server.listen(3000, function(){
